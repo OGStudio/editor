b41622d23e582d1a7a9b670ea8c72e2f9f9e680e
Michael Kapelko <kornerr@gmail.com>
2016-03-19 15:33:06 +0700

    #2338 Implement creation of a new project

--- scripts/CameraSync.py
+++ scripts/CameraSync.py
@@ -30,7 +30,7 @@ class CameraSyncImpl(object):
             return
         self.nodeToCamera()
     def setSync(self, key, value):
-        print "setSync", key, value
+        #print "setSync", key, value
         src = value[0]
         if (src == "node"):
             self.nodeToCamera()
@@ -45,20 +45,20 @@ class CameraSync(object):
         # Prepare.
         self.c.setConst("SCENE", sceneName)
         self.c.setConst("NODE",  nodeName)
-        print "CameraSync. node name:", nodeName
+        #print "CameraSync. node name:", nodeName
         self.impl.nodeToCamera()
         self.c.provide("camera.sync", self.impl.setSync)
         # Listen to node orientation changes.
         self.c.listen("node.$SCENE.$NODE.position", None, self.impl.setPosition)
         self.c.listen("node.$SCENE.$NODE.rotation", None, self.impl.setRotation)
-        print "{0} CameraSync.__init__({1}, {2})".format(id(self), sceneName, nodeName)
+        #print "{0} CameraSync.__init__({1}, {2})".format(id(self), sceneName, nodeName)
     def __del__(self):
         # Tear down.
         self.c.clear()
         # Destroy
         del self.impl
         del self.c
-        print "{0} CameraSync.__del__".format(id(self))
+        #print "{0} CameraSync.__del__".format(id(self))
 
 def SCRIPT_CREATE(sceneName, nodeName, env):
     return CameraSync(sceneName, nodeName, env)
